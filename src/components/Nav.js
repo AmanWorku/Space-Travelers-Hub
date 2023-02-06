47194052d357abadf83fe26a51be93dfac86fff2
Omar Farouq <ofarouq310@gmail.com>
2023-02-06 12:32:52 +0200

    Add navigation bar component

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+export default function Nav() {
+  return (
+    <div className="nav--bar">
+      <h1>Space travelers Hub</h1>
+    </div>
+  );
+}
