d0a1c6b9dc2a464328477e3d8eaa5fc62cc00130
edenlisk <nkumbuyenock@gmail.com>
2023-02-07 12:28:54 +0200

    Fetch rockets data

---
+++ src/components/Rocket.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Rocket = (props) => {
+  const { name, description, image } = props;
+
+  return (
+    <div className="card mb-3">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={image} className="bd-placeholder-img img-fluid rounded-start" width="100%" height="262" alt={name} />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{description}</p>
+            <button type="button" className="btn btn-primary my-5">Reserve Rocket</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Rocket.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default Rocket;
