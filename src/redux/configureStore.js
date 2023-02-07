5838392df41dfea59c64073f0766d8d92b004b9b
AmanWorku <amexworku2014@gmail.com>
2023-02-07 17:44:19 +0300

    Add missions reducer to the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import thunk from 'redux-thunk';
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
 import rockets from './rockets/rockets';
+import missions from './missions/missions';
 
 const reducers = combineReducers({
   rocketsReducer: rockets,
+  missionsReducer: missions,
 });
 
 const store = createStore(reducers, applyMiddleware(thunk));
